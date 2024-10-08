dada01a01aac53ee2fcfb3c3804ed8287d881789
bachexa17 <bacho.begiashvili@gmail.com>
2024-10-08 05:47:43 -0800

    finished content option scroll style and js

--- scripts/app.js
+++ scripts/app.js
@@ -12,17 +12,38 @@ function contentOptRender() {
 
 function contentOptScroll() {
   const scrollElement = document.querySelector('.options-box');
-  const elementWidth = scrollElement.clientWidth;
-  const scrollSize = (0.10 * elementWidth);
-  let scrollState = 0;
+  const scrollElementWidth = scrollElement.scrollWidth - scrollElement.clientWidth;
+  const scrollWidth = (scrollElementWidth / 100) * 25;
+  let scrollState = 0
+  scrollElement.scrollLeft = 0;
+  checkArrow();
 
   document.querySelector('.right-arrow-img').addEventListener('click', () => {
-    scrollElement.style.transform = `translateX(${scrollState -= scrollSize}px)`;
+    scrollElement.scrollLeft = scrollState += scrollWidth;
+    checkArrow();
   });
 
   document.querySelector('.left-arrow-img').addEventListener('click', () => {
-    scrollElement.style.transform = `translateX(${scrollState += scrollSize}px)`;
+    scrollElement.scrollLeft = scrollState -= scrollWidth;
+    checkArrow();
   });
+
+  function checkArrow() {
+    const rightArrowElement = document.querySelector('.right-arrow');
+    const leftArrowElement = document.querySelector('.left-arrow');
+    console.log(scrollElement.scrollLeft)
+    console.log(scrollState)
+
+    rightArrowElement.style.visibility = 'visible';
+    leftArrowElement.style.visibility = 'hidden';
+
+    if (scrollState === scrollElementWidth) {
+      rightArrowElement.style.visibility = 'hidden';
+    };
+    if (scrollState > 0) {
+      leftArrowElement.style.visibility = 'visible';
+    };
+  };
 
 };
 
